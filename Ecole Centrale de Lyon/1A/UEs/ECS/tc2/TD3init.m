a54f32d562e27a7c0edb18a324c67c01bb8777a1
Paco TANCHON <paco.tanchon@ecl17.ec-lyon.fr>
2018-03-11 15:31:43 +0100

    Uplink PC Portable

---
+++ Ecole Centrale de Lyon/1A/UEs/ECS/tc2/TD3init.m
@@ -0,0 +1,25 @@
+%initialisation des paramètres pour le fichier TD6.mdl
+
+
+%paramètres du procédé
+k=1.2;
+T1=155;
+T2=2;
+
+%paramètres du correcteur analogique
+kc=4.2;
+Ti=273;
+Td=51;
+Tfi=37;
+
+%paramètres du correcteur numérique
+%remplacer la valeur de Te et les 0 et 1 par des valeurs pertinentes
+Te=60;
+a1=0;
+a2=0;
+b1=1;
+b2=1;
+c1=0;
+c2=0;
+d1=1;
+d2=1;
